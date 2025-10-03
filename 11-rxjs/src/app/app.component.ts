435d05f38ef9daf3ef70a0dffb8561d367f9b32b
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-10-02 23:39:40 -0400

    11-rxjs | Add a clickCount signal for the button event

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,23 +8,35 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit{
   private destroRef = inject(DestroyRef);
+  clickCount = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log(`click button ${this.clickCount()} times `);
+
+    })
+  }
   ngOnInit(): void {
-    //this is another way to pass an object to obtain more than next, like error or complete
-    const subscription = interval(1000)
-    .pipe(
-      map((val:number) => val * 10),
-      map((val:number) => val - 1)
-    )
-    .subscribe({ //interval is a observable than return a number every millisecond we expecify
-      next: (val: number) => console.log(val),
-      complete: (()=> console.log('completed')
-      )
-    })
+    // //this is another way to pass an object to obtain more than next, like error or complete
+    // const subscription = interval(1000)
+    // .pipe(
+    //   map((val:number) => val * 10)
+    // )
+    // .subscribe({ //interval is a observable than return a number every millisecond we expecify
+    //   next: (val: number) => console.log(val),
+    //   complete: (()=> console.log('completed')
+    //   )
+    // })
 
 
 
-    this.destroRef.onDestroy(() => { //Here is another way to use the onDestroy but without having the method
-      subscription.unsubscribe();
-    })
+    // this.destroRef.onDestroy(() => { //Here is another way to use the onDestroy but without having the method
+    //   subscription.unsubscribe();
+    // })
+  }
+
+
+  onClick(): void {
+    this.clickCount.update(prevVal => prevVal + 1)
   }
 }
