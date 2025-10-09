aa16c6ee967dd89cfe6127bbbde6057f9709080a
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-10-08 23:15:44 -0400

    12-http | How To use the HttpClient with the new standalone components

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +12,20 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit{
+  private httpClient = inject(HttpClient);
   places = signal<Place[] | undefined>(undefined);
+  private destroRef = inject(DestroyRef)
+
+
+  ngOnInit(): void {
+    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places').subscribe(val => {
+      console.log(val);
+
+    })
+
+    this.destroRef.onDestroy(()=> {
+      subscription.unsubscribe();
+    })
+  }
 }
