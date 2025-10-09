ee8e694b18e1720f1e2433925bb59e494980545a
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-10-08 23:45:15 -0400

    12-http | Handling errors in http requests

--- 12-http/src/app/places/available-places/available-places.component.ts
+++ 12-http/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { catchError, pipe, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -15,18 +16,31 @@ import { HttpClient } from '@angular/common/http';
 export class AvailablePlacesComponent implements OnInit{
   private httpClient = inject(HttpClient);
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false)
+  isFail = signal(false)
   private destroRef = inject(DestroyRef)
 
 
   ngOnInit(): void {
-    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places', {
-      observe: 'response' // or 'events' to get other information
-    }).subscribe(val => {
-      console.log(val);
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
+    .pipe(
+        catchError((error) => throwError(()=> new Error('Something fails in the server!')))
+    )
+    .subscribe(
 
-      console.log(val.body?.places);
-
-    })
+      {
+        next: val => {
+          console.log(val);
+          this.places.set(val.places);
+        },
+        complete: () => {
+          this.isFetching.set(false)
+        },
+        error: (err) =>{
+          this.isFail.set(err)
+        }
+      })
 
     this.destroRef.onDestroy(()=> {
       subscription.unsubscribe();
