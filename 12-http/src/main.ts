c604f7ef93d2a4e4069b6c9c69ea090c221db3e3
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-10-10 00:23:07 -0400

    12-http | How Can we work with interceptors with the new Angular version +19, Now intercepting the response

--- 12-http/src/main.ts
+++ 12-http/src/main.ts
@@ -1,16 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
   console.log('[Outgoing Request]');
   console.log(request);
-  const req = request.clone({
-    headers: request.headers.set('X-DEBUG',' TESTING') //this give the actual header plus one ".set"
-  })
 
-  return next(req)
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if(event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.body);
+        }
+      }
+    })
+  )
 }
 
 bootstrapApplication(AppComponent, {
