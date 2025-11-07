162071379aff8cde0770d01c854e67c84b1d8f66
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-11-06 22:20:37 -0500

    14-routing | adding mix things

---
+++ 14-routing/src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { TaskComponent } from './tasks/task/task.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: NoTaskComponent,
+  },
+  {
+    path: 'users/:userId',
+    component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks', //<your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent
+      }
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+
+
+];
--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input, OnChanges, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
-  templateUrl: './user-tasks.component.html',
-  styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './user-tasks.component.html', styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit{
+  userId = input.required<string>();
+  private userService = inject(UsersService);
+  private activatedRout = inject(ActivatedRoute);
+
+  userName = computed(()=> this.userService.users.find(user => user.id === this.userId()))
+
+
+  ngOnInit(): void {
+
+  }
+}
