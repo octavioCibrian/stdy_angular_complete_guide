09a00e348783f195c37b32e1921ff8d0b0157175
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-11-06 22:58:46 -0500

    14-routing | children routes

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Routes = [
   {
@@ -14,16 +15,8 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      {
-        path: 'tasks', //<your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent
-      }
-    ],
+    children: userRoutes
+
   },
   {
     path: '**',
