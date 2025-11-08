9f6192260fef535bf48c365f1875e754c3172df0
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-11-07 19:51:00 -0500

    14-routing | query with observables

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,9 +1,10 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
@@ -12,11 +13,25 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>();
+  // order = input<'asc' | 'desc'>();
+  order?: 'asc' | 'desc';
+
+  private destroyRef = inject(DestroyRef);
 
   private tasksService = inject(TasksService);
   userTasks = computed(() => this.tasksService.allTasks().filter((task: any) => task.userId === this.userId()))
 
+  private activateRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const subscription = this.activateRoute.queryParams.subscribe((params)=> {
+      this.order = params['order']
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
