286d5de216359dc6b8a6e178248aa76d909f2399
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-11-11 17:17:07 -0500

    14-routing | using resolvers

--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject, input, OnChanges, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterOutlet, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterOutlet, RouterLink, ResolveFn, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from '../user/user.model';
 
 @Component({
   selector: 'app-user-tasks',
@@ -8,15 +9,14 @@ import { ActivatedRoute, RouterOutlet, RouterLink } from '@angular/router';
   imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html', styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit{
-  userId = input.required<string>();
-  private userService = inject(UsersService);
-  private activatedRout = inject(ActivatedRoute);
+export class UserTasksComponent {
+  userName = input.required<User>();
 
-  userName = computed(()=> this.userService.users.find(user => user.id === this.userId()))
+}
 
-
-  ngOnInit(): void {
-
-  }
+export const resolveUserName:ResolveFn<string> = (activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot) => {
+  const userService = inject(UsersService)
+  const userName = userService.users.find(user => user.id === activatedRoute.paramMap.get('userId'))
+  ?.name || '';
+  return userName;
 }
